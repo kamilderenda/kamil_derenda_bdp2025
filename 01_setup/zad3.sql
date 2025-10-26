6171e58cbbcd98aef972323cdb744030d271be13
Kamil Derenda <141368893+kamilderenda@users.noreply.github.com>
2025-10-26 21:15:53 +0100

    EWKT
    Added SQL commands for creating tables, inserting data, and performing spatial queries.

--- 01_setup/zad3.sql
+++ 01_setup/zad3.sql
@@ -1,13 +1,108 @@
 ZAD 3. EWKT
-1) CREATE TABLE obiekty(
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	polygons GEOMETRY(POLYGON),
-	lines GEOMETRY(LINESTRING),
-	points GEOMETRY(POINT)
-)
-
 a)
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+CREATE TABLE IF NOT EXISTS obiekty(
+    id serial PRIMARY KEY,
+    nazwa text,
+    geom geometry
+);
+
+INSERT INTO obiekty (nazwa, geom)
+VALUES ('obiekt1',
+  ST_Collect(ARRAY[
+    ST_MakeLine(ST_MakePoint(0,1), ST_MakePoint(1,1)), 
+    ST_GeomFromText('CIRCULARSTRING(1 1, 2 0, 3 1)'), 
+    ST_GeomFromText('CIRCULARSTRING(3 1, 4 2, 5 1)'),
+    ST_MakeLine(ST_MakePoint(5,1), ST_MakePoint(6,1))      
+  ])
+);
+
+b)
+WITH outer_ring AS (
+  SELECT ST_MakePolygon(
+    ST_LineMerge(ST_Collect(ARRAY[
+      ST_MakeLine(ST_MakePoint(10,6), ST_MakePoint(14,6)),
+      ST_GeomFromText('CIRCULARSTRING(14 6, 16 4, 14 2)'),
+      ST_GeomFromText('CIRCULARSTRING(14 2, 12 0, 10 2)'),
+      ST_MakeLine(ST_MakePoint(10,2), ST_MakePoint(10,6))
+    ]))
+  ) AS geom
+),
+inner_hole AS (
+  SELECT ST_Buffer(ST_MakePoint(12,2), 1) AS geom
+)
+INSERT INTO obiekty (nazwa, geom)
+SELECT 
+  'obiekt2',
+  ST_Difference(outer_ring.geom, inner_hole.geom)
+FROM outer_ring, inner_hole;
+
+
+c)
+INSERT INTO obiekty(nazwa, geom)
+VALUES ('obiekt3',
+  ST_GeomFromText('POLYGON((7 15, 10 17, 12 13, 7 15))')
+);
+
+d)
+INSERT INTO obiekty(nazwa, geom)
+VALUES ('obiekt4',
+  ST_GeomFromText('LINESTRING(20 20, 25 25, 27 24, 25 22, 26 21, 22 19, 20.5 19.5)')
+);
+
+e)
+INSERT INTO obiekty(nazwa, geom)
+VALUES ('obiekt5',
+  ST_Collect(ARRAY[
+    ST_GeomFromText('POINT Z(30 30 59)'),
+    ST_GeomFromText('POINT Z(38 32 234)')
+  ])
+);
+
+f)
+INSERT INTO obiekty(nazwa, geom)
+VALUES ('obiekt6',
+  ST_Collect(ARRAY[
+    ST_GeomFromText('LINESTRING(1 1, 3 2)'),
+    ST_GeomFromText('POINT(4 2)')
+  ])
+);
+
+
+
+2. 
+SELECT ST_Area(
+         ST_Buffer(
+           ST_ShortestLine(
+             (SELECT geom FROM obiekty WHERE nazwa='obiekt3'),
+             (SELECT geom FROM obiekty WHERE nazwa='obiekt4')
+           ),
+           5
+         )
+       ) AS pole_powierzchni;
+
+3. 
+UPDATE obiekty
+SET geom = ST_MakePolygon(
+  ST_AddPoint(geom, ST_StartPoint(geom))
+)
+WHERE nazwa = 'obiekt4';
+4. 
+INSERT INTO obiekty (nazwa, geom)
+SELECT 'obiekt7',
+       ST_Union(
+         (SELECT geom FROM obiekty WHERE nazwa = 'obiekt3'),
+         (SELECT geom FROM obiekty WHERE nazwa = 'obiekt4')
+       );
+
+5. 
+SELECT SUM(ST_Area(ST_Buffer(geom, 5))) AS suma_pol_buforow
+FROM obiekty
+WHERE NOT ST_HasArc(geom);
+
+
+
 
 ZAD 3. HERE
 1. SELECT left_table.*
@@ -95,3 +190,4 @@ INTO T2019_KAR_BRIDGES
 FROM T2019_KAR_RAILWAYS AS left_table
 CROSS JOIN T2019_KAR_WATER_LINES AS right_table
 WHERE ST_Intersects(left_table.geom, right_table.geom)
+
